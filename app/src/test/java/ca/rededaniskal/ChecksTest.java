748694d502a64d66400e327159fd99bedc38410b
Delaney <dlothian@ualberta.ca>
2019-02-22 16:52:58 -0700

    tests related to BookExchange in ChecksTest

--- app/src/test/java/ca/rededaniskal/ChecksTest.java
+++ app/src/test/java/ca/rededaniskal/ChecksTest.java
@@ -7,53 +7,68 @@
 package ca.rededaniskal;
 
 import java.util.Date;
+import static org.junit.Assert.*;
 
 public class ChecksTest {
 
-    public String Location() {
-        return location;
+    // The following tests test the getters and setters for the attributes declared in BookExchange
+
+    public void Location() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        checkin.setLocation("Edmonton, AB");
+        String loc = checkin.getLocation();
+        assertEquals("Edmonton, AB", loc);
     }
 
 
-    public Date getTime() {
-        return time;
+    public void Time() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        Date time = new Date();
+        checkin.setTime(time);
+        Date timetest = checkin.getTime();
+        assertEquals(time, timetest);
     }
 
-    public void setTime(Date time) {
-        this.time = time;
+
+    public void Owner() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        String owner = checkin.getOwner();
+        assertEquals("dlothian", owner);
+        checkin.setOwner("revanMac");
+        String owner2 = checkin.getOwner();
+        assertEquals("revan", owner2);
     }
 
-    public String getOwner() {
-        return owner;
+
+    public void Borrower() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        String borrower = checkin.getBorrower();
+        assertEquals("daniela", borrower);
+        checkin.setBorrower("revanMac");
+        String borrower2 = checkin.getBorrower();
+        assertEquals("revan", borrower2);
     }
 
-    public void setOwner(String owner) {
-        this.owner = owner;
+
+    public void Isbn() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        String isbn = checkin.getIsbn();
+        assertEquals("123456", isbn);
+        checkin.setIsbn("54321");
+        String isbn2 = checkin.getIsbn();
+        assertEquals("revan", isbn2);
     }
 
-    public String getBorrower() {
-        return borrower;
+
+    public void Bookid() {
+        CheckIn checkin = new CheckIn("dlothian", "daniela", "123456", 7);
+        int bookid = checkin.getBookid();
+        assertEquals(7, bookid);
+        checkin.setBookid(8);
+        int bookid2 = checkin.getBookid();
+        assertEquals(8, bookid2);
     }
 
-    public void setBorrower(String borrower) {
-        this.borrower = borrower;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getBookid() {
-        return bookid;
-    }
-
-    public void setBookid(int bookid) {
-        this.bookid = bookid;
-    }
 
     public void scanISBN(){}
 }
