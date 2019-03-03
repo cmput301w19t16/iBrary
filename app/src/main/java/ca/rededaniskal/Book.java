d74e31558d4492062c1fa4f01c012aa22ac67c57
Nicholas Bombardieri <33304097+nichbomb@users.noreply.github.com>
2019-03-03 14:05:58 -0700

    Fixed Merge Conflics

--- app/src/main/java/ca/rededaniskal/Book.java
+++ app/src/main/java/ca/rededaniskal/Book.java
@@ -1,25 +1,10 @@
 package ca.rededaniskal;
 
 public class Book {
-<<<<<<< HEAD
-    private String Title;
-    private String Author;
-    private String ISBN;
-    private String Description;
-
-    public Book(String title, String author, String isbn, String description){
-        Title = title;
-        Author = author;
-        ISBN = isbn;
-        Description = description;
-    }
-=======
 
     private String title;
     private String author;
     private String isbn;
->>>>>>> 09d679e34079705f54c57f34a6dacb0c44de23f9
-
 
     public Book(String newTitle, String newAuthor, String newIsbn){
         title = newTitle;
