1638f354c51519dea16c57323beb30c5a68344d5
dteodore <dteodore@ualberta.ca>
2019-03-04 19:01:42 -0700

    Begin working user_details_activity

--- app/src/main/java/ca/rededaniskal/User_Details_Activity.java
+++ app/src/main/java/ca/rededaniskal/User_Details_Activity.java
@@ -9,14 +9,17 @@ import android.widget.Button;
 
 public class User_Details_Activity extends AppCompatActivity {
 
-
+    private User user_received;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__details_);
-
-
-
+        
+        Intent intent = getIntent();
+        Bundle bundle_extras = intent.getExtras();
+        if (bundle_extras != null){
+            user_received = (User) getIntent().getSerializableExtra("KEY");
+        }
     }
 }
