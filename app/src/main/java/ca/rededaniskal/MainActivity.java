619e2abba26a8901e236884c06617261ea30033b
dteodore <dteodore@ualberta.ca>
2019-03-07 00:22:19 -0700

    Fixed gradle

--- app/src/main/java/ca/rededaniskal/MainActivity.java
+++ app/src/main/java/ca/rededaniskal/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import ca.rededaniskal.Barcode_Scanner_Activity;
-
-
 public class MainActivity extends AppCompatActivity{
 
     @Override
