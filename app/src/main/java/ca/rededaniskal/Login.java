062f90f3b6eb3b65763084a87121a377e26320df
Revan MacQueen <revan.macqueen1@gmail.com>
2019-03-07 18:36:15 -0700

    Abt to merge w/ delaney

--- app/src/main/java/ca/rededaniskal/Login.java
+++ app/src/main/java/ca/rededaniskal/Login.java
@@ -1,29 +1,56 @@
 package ca.rededaniskal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    Button loginButton;
+    Button RegisterButton;
+    EditText name;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar); TODO: ???
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Set the buttons and Edit Texts
+        loginButton = (Button) findViewById(R.id.button);
+        RegisterButton = (Button) findViewById(R.id.button2);
+        name  = (EditText) findViewById(R.id.editText5);
+        password = (EditText)findViewById(R.id.editText6);
+
+        //Set on click listeners
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+
+
+                //TODO: Database stuff
+
             }
         });
+
+        //Set on click listeners
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
 }
