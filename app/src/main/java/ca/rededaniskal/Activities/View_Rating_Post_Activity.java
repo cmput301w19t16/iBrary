b0f624a9f476e5f268e708d27d510ad4eb165ae8
dteodore <dteodore@ualberta.ca>
2019-03-08 17:23:13 -0700

    UI for View_Rating_Post

---
+++ app/src/main/java/ca/rededaniskal/Activities/View_Rating_Post_Activity.java
@@ -0,0 +1,31 @@
+package ca.rededaniskal.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ca.rededaniskal.R;
+
+public class View_Rating_Post_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__rating__post_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
