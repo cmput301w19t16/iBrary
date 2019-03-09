7a14d5089b2a256795ddc5df4699d5565c55c667
dteodore <dteodore@ualberta.ca>
2019-03-08 23:59:03 -0700

    Working search UI and cleaned up

--- app/src/main/java/ca/rededaniskal/Activities/Search_Activity.java
+++ app/src/main/java/ca/rededaniskal/Activities/Search_Activity.java
@@ -1,96 +1,16 @@
-//Tutorial for selecting more than one option in search
-//https://www.youtube.com/redirect?q=https%3A%2F%2Fgithub.com%2Fcodingdemos%2FMultichoiceTutorial&redir_token=zWJM5OoUtOrwMvfLlGWm1qv4-B98MTU1MjE5NTgxMEAxNTUyMTA5NDEw&event=video_description&v=wfADRuyul04
 package ca.rededaniskal.Activities;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.ArrayList;
-
 import ca.rededaniskal.R;
 
 public class Search_Activity extends AppCompatActivity {
-
-    /*
-    Button searchBy;
-    String[] filterOptions;
-    boolean[] selectedOptions;
-    ArrayList<Integer> chosenOptions = new ArrayList<>();*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_);
-
-        /*searchBy = (Button) findViewById(R.id.FilterSearchButton);
-        filterOptions = getResources().getStringArray(R.array.filter_search_options);
-        selectedOptions = new boolean[filterOptions.length];
-
-
-        searchBy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(Search_Activity.this);
-                builder.setTitle(R.string.search_by);
-                builder.setMultiChoiceItems(filterOptions, selectedOptions, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
-                        if(isChecked){
-                            chosenOptions.add(position);
-                        }else{
-                            chosenOptions.remove((Integer.valueOf(position)));
-                        }
-                    }
-                });
-
-                builder.setCancelable(false);
-                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        String item = "";
-                        for (int i = 0; i < chosenOptions.size(); i++) {
-                            item = item + filterOptions[chosenOptions.get(i)];
-                            if (i != chosenOptions.size() - 1) {
-                                item = item + ", ";
-                            }
-                        }
-                        //mItemSelected.setText(item);
-                    }
-                });
-
-                builder.setNegativeButton(R.string.dismiss, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                builder.setNeutralButton(R.string.clear_all, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        for (int i = 0; i < selectedOptions.length; i++) {
-                            selectedOptions[i] = false;
-                            chosenOptions.clear();
-                            //mItemSelected.setText("");
-                        }
-                    }
-                });
-
-                AlertDialog mDialog = builder.create();
-                mDialog.show();
-            }
-        });
-*/
-
-
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
