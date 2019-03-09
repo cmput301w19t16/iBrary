770bb1ac1a3c5a6c4226fe0ea1d1be7a877c4615
Nicholas Bombardieri <nbombard@ualberta.ca>
2019-03-09 12:15:29 -0700

    made posts in feed look nice

--- app/src/main/java/ca/rededaniskal/BusinessLogic/PostAdapter.java
+++ app/src/main/java/ca/rededaniskal/BusinessLogic/PostAdapter.java
@@ -1,9 +1,6 @@
 package ca.rededaniskal.BusinessLogic;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ca.rededaniskal.Activities.Fragments.Post_Feed_Fragment;
-import ca.rededaniskal.Activities.Fragments.View_Own_Profile_Fragment;
-import ca.rededaniskal.Activities.Login_Activity;
 import ca.rededaniskal.Activities.View_Rating_Post_Activity;
 import ca.rededaniskal.Activities.View_Text_Post_Activity;
 import ca.rededaniskal.EntityClasses.Post;
@@ -27,7 +22,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     public Post_Feed_Fragment fragment;
 
     public class PostViewHolder extends RecyclerView.ViewHolder{
-        public TextView textView;
+        public TextView postTitle;
+        public TextView postBodyText;
         public String postType;
         public String postID;
         public View view;
@@ -35,7 +31,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         public PostViewHolder(View v){
             super(v);
             view = v;
-            textView = v.findViewById(R.id.post_text_view);
+            postTitle = v.findViewById(R.id.post_title);
+            postBodyText = v.findViewById(R.id.post_body_text);
         }
     }
 
@@ -55,20 +52,23 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     @Override
     public void onBindViewHolder(final PostViewHolder holder, int position){
         Post post = mDataset.get(position);
-        String text = post.getUserName();
+        String titleText = post.getUserName();
+        String bodyText = "";
         switch (post.getType()){
             case "Rating_Post":
-                text += " just reviewed " + post.getISBN();
-
+                titleText += " just reviewed " + post.getISBN();
+                bodyText += post.getMessage();
                 break;
             case "Text_Post":
-                text += " made a post about " + post.getISBN();
+                titleText += " made a post about " + post.getISBN();
+                bodyText += post.getMessage();
                 break;
             case "alert":
-                text = post.getMessage();
+                titleText = post.getMessage();
         }
 
-        holder.textView.setText(text);
+        holder.postTitle.setText(titleText);
+        holder.postBodyText.setText(bodyText);
         holder.postType = post.getType();
         holder.postID = post.getID();
 
@@ -82,13 +82,14 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
                         break;
                     case "Rating_Post":
                         intent = new Intent(fragment.getContext(), View_Rating_Post_Activity.class);
+                        fragment.startActivity(intent);
                         break;
                     default:
-                        intent = new Intent(fragment.getContext(), Login_Activity.class);
+                        Toast.makeText(fragment.getActivity(), "All out of posts. " +
+                                "Refresh for more!", Toast.LENGTH_LONG).show();
                         break;
 
             }
-            fragment.startActivity(intent);
         }
     });
 
