f516de3f89936776c564af5596c7e809c017c48d
Revan MacQueen <revan.macqueen1@gmail.com>
2019-03-09 13:12:22 -0700

    Camera For adding book done

--- app/src/main/java/ca/rededaniskal/Activities/Add_Book_To_Library_Activity.java
+++ app/src/main/java/ca/rededaniskal/Activities/Add_Book_To_Library_Activity.java
@@ -2,6 +2,13 @@ package ca.rededaniskal.Activities;
 
 /*author : Skye*/
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +16,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,15 +25,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import ca.rededaniskal.AddBookLogic;
+import ca.rededaniskal.Barcode.Barcode_Scanner_Activity;
 import ca.rededaniskal.EntityClasses.Book_Instance;
 
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import ca.rededaniskal.Database.BookInstanceDetailsDatabase;
-import ca.rededaniskal.EntityClasses.Book_Instance;
 import ca.rededaniskal.R;
 
 public class Add_Book_To_Library_Activity extends AppCompatActivity {
@@ -34,12 +36,17 @@ public class Add_Book_To_Library_Activity extends AppCompatActivity {
     private DatabaseReference bookRef;
 
     //UI stuff
-
     private EditText addTitle, addAuthor, addISBN, addDescription;
     private Button openScanner, addBook;
     private FloatingActionButton openCamera;
+    private ImageView cover;
 
     private AddBookLogic businessLogic;
+
+    //For Camera
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +61,31 @@ public class Add_Book_To_Library_Activity extends AppCompatActivity {
         openScanner = findViewById(R.id.openScanner);
         openCamera = findViewById(R.id.openCamera);
         addBook = findViewById(R.id.addBook);
+
+        cover = findViewById(R.id.BookCover);
+
+        openScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Barcode_Scanner_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -109,6 +141,29 @@ public class Add_Book_To_Library_Activity extends AppCompatActivity {
             else{
                 Toast.makeText(this, "Database Error!", Toast.LENGTH_SHORT);
             }
+        }
+    }
+
+    //Code From https://stackoverflow.com/a/5991757
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new
+                        Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            cover.setImageBitmap(photo);
         }
     }
 }
