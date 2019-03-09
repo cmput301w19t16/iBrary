c2888cb18fa6acae1fb044788ebac2322adc4cb7
Skye Hyndman <shyndman@ualberta.ca>
2019-03-09 06:30:56 -0700

    DataProvider class emulating a content Provider (Book Instance Updates)

--- app/src/main/java/ca/rededaniskal/Database/Data_Provider.java
+++ app/src/main/java/ca/rededaniskal/Database/Data_Provider.java
@@ -1,53 +1,199 @@
 package ca.rededaniskal.Database;
-
-import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.net.Uri;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+//reference :  https://www.youtube.com/watch?v=zVjSnhJu9qw
 
 
-public class Data_Provider extends ContentProvider {
+import android.support.annotation.NonNull;
 
 
-    @Override
-    public boolean onCreate() {
-        FirebaseOptions.Builder builder = new FirebaseOptions.Builder()
-                .setApplicationId("1:212907668448:android:01929685eb1c815e")
-                .setApiKey("AIzaSyAKjxhAk_vkugEaKR2DmfGS9v6A-WY0Nco")
-                .setDatabaseUrl("https://ibrary.firebaseio.com")
-                .setStorageBucket("ibrary.appspot.com");
-        FirebaseApp.initializeApp(getContext(), builder.build());
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ca.rededaniskal.EntityClasses.Book_Instance;
+import ca.rededaniskal.EntityClasses.Book_List;
+
+import static com.google.firebase.database.FirebaseDatabase.getInstance;
+
+
+public class Data_Provider {
+
+    private Book_Instance book_instance;
+
+
+
+
+
+
+    static final String owner ="owner";
+    static final String author= "author";
+    static final String possessor = "possessor";
+    static final String title = "title";
+    static final String isbn = "isbn";
+
+    //Hash Map to keep track of the data structure tree
+
+
+
+    private HashMap<Integer, String> urls;
+
+    public Data_Provider() {
+        urls = new HashMap<>();
+        urls.put(-1, "master-books");
+        urls.put(2, "book-instances");
+        urls.put(4, "book-status/a");
+        urls.put(6, "book-status/b");
+        urls.put(8, "book-status/r");
+        urls.put(10, "book-status/c");
+        urls.put(34, "user/user-books");
+        urls.put(17, "user/user-posts");
+        urls.put(19, "users");
+        urls.put(23, "posts");
+        urls.put(14, "requests");
+
+
+
+    }
+
+
+
+
+
+    public Book_List bookQuery(String url, String selection, String[] selectionArgs, String sortOrder) {
+        DatabaseReference bookRef= FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(urls.get(url));
+
+
+        return null;
+
+
+
+    }
+
+    public Book_Instance getBook(Integer url, final String bookID){
+        if (url%2!=0 || url%17==0) return null;
+        else{
+            DatabaseReference bookRef = FirebaseDatabase.getInstance().getReference().child(urls.get(url));
+
+           bookRef.addListenerForSingleValueEvent(new ValueEventListener() {
+               @Override
+               public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                   if (dataSnapshot.hasChild(bookID)){
+                       Book_Instance book = dataSnapshot.getValue(Book_Instance.class);
+                       book.setBookID(bookID);
+                       setBook_instance(book);
+
+
+                   }
+               }
+
+               @Override
+               public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+               }
+           });
+
+        }
+
+        return this.book_instance;
+
+
+    }
+
+
+
+
+
+    public  boolean insert(Book_Instance book_instance) {
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference newRef = databaseReference.child(urls.get(2));
+        String id = newRef.push().getKey();
+        if (!newRef.child(id).setValue(book_instance).isSuccessful()){
+            return false;
+        };
+        
+        for (Integer i: urls.keySet()){
+            newRef = databaseReference.child(urls.get(i));
+            if (i%2 ==0 && i>2) {
+                if (!newRef.child(id).setValue(book_instance).isSuccessful()){
+                    return  false;
+                }
+
+
+
+                if (i%17 ==0){
+                    newRef.child(user).child(id).setValue(book_instance);
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+
+
+
+
         return false;
     }
 
-    @androidx.annotation.Nullable
-    @Override
-    public Cursor query(@androidx.annotation.NonNull Uri uri, @androidx.annotation.Nullable String[] projection, @androidx.annotation.Nullable String selection, @androidx.annotation.Nullable String[] selectionArgs, @androidx.annotation.Nullable String sortOrder) {
-        return null;
+
+    public Integer deleteBookInstance(Integer url, String id) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        for (Integer i: urls.keySet()){
+            if (i%2 ==0) {
+                DatabaseReference newRef = databaseReference.child(urls.get(i));
+                if (i%17 ==0){
+                    newRef.child(user).child(id).removeValue();
+                }
+                else if (i%7 ==0){
+                    //TODO
+                }
+                else{
+                    newRef.child(id).removeValue();
+                }
+
+            }
+            else{
+                return null;
+            }
+
+
+        }
+
+        return 1;
+
     }
 
-    @androidx.annotation.Nullable
-    @Override
-    public String getType(@androidx.annotation.NonNull Uri uri) {
-        return null;
-    }
 
-    @androidx.annotation.Nullable
-    @Override
-    public Uri insert(@androidx.annotation.NonNull Uri uri, @androidx.annotation.Nullable ContentValues values) {
-        return null;
-    }
+    public Integer update() {
 
-    @Override
-    public int delete(@androidx.annotation.NonNull Uri uri, @androidx.annotation.Nullable String selection, @androidx.annotation.Nullable String[] selectionArgs) {
+
         return 0;
+
+
+        }
+
+
+    public void setBook_instance(Book_Instance book){
+        this.book_instance = book;
     }
 
-    @Override
-    public int update(@androidx.annotation.NonNull Uri uri, @androidx.annotation.Nullable ContentValues values, @androidx.annotation.Nullable String selection, @androidx.annotation.Nullable String[] selectionArgs) {
-        return 0;
-    }
+
 }
