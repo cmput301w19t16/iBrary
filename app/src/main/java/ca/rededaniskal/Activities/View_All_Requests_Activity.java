2e5e87bed62cbaa25963d4c603fe46de98c04943
dteodore <dteodore@ualberta.ca>
2019-03-10 00:41:30 -0700

    See all requests

--- app/src/main/java/ca/rededaniskal/Activities/View_All_Requests_Activity.java
+++ app/src/main/java/ca/rededaniskal/Activities/View_All_Requests_Activity.java
@@ -10,23 +10,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
+import ca.rededaniskal.BusinessLogic.AllRequestsAdapter;
 import ca.rededaniskal.BusinessLogic.BookAdapter;
 import ca.rededaniskal.EntityClasses.Book_Instance;
 import ca.rededaniskal.EntityClasses.Book_List;
+import ca.rededaniskal.EntityClasses.Request;
 import ca.rededaniskal.R;
+
+/**
+ * @author Daniela, Revan
+ */
 
 public class View_All_Requests_Activity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private BookAdapter bookAdapter;
+    private AllRequestsAdapter adapter;
+    private ArrayList<Request> requestList = new ArrayList<Request>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //For testing
-        //TODO: DB load the libary of the current user
-        Book_List BL = new Book_List();
-        Book_Instance HP = new Book_Instance("HappyPotter", "JK", "123", "Revan", "Daniela", "Very Nice", "Requested");
-        BL.addBook(HP);
+        //TODO: DB load the libary of requested books for the current user
+
+        Request request1 = new Request("Daniela", "Skye", "book");
+        Request request2 = new Request("Daniela", "Nick", "book");
+        requestList.add(request1);
+        requestList.add(request2);
+
+       // Book_Instance HP = new Book_Instance("HappyPotter", "JK", "123", "Revan", "Daniela", "Very Nice", "Requested");
+        //BL.addBook(HP);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view__all__requests_);
@@ -35,9 +49,9 @@ public class View_All_Requests_Activity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        bookAdapter = new BookAdapter(this, BL);
-        recyclerView.setAdapter(bookAdapter);
-        bookAdapter.notifyDataSetChanged();
+        adapter = new AllRequestsAdapter(this, requestList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
