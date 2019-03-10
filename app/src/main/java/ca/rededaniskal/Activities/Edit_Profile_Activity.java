d5efb0fb7048cf4dfbe42e68afd920daf9b7546f
Delaney <dlothian@ualberta.ca>
2019-03-10 14:18:26 -0600

    autofill with user information for edit profile

--- app/src/main/java/ca/rededaniskal/Activities/Edit_Profile_Activity.java
+++ app/src/main/java/ca/rededaniskal/Activities/Edit_Profile_Activity.java
@@ -11,13 +11,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.rededaniskal.EntityClasses.User;
 import ca.rededaniskal.R;
+
+import static android.content.ContentValues.TAG;
 
 public class Edit_Profile_Activity extends AppCompatActivity {
     Button saveButton;
@@ -25,6 +42,7 @@ public class Edit_Profile_Activity extends AppCompatActivity {
     ImageView profilePicture;
     EditText newUsername, newPhone, newEmail, newLocation,
             oldPassword, newPassword, confirmNewPassword;
+    private editUserDetailsDB db;
 
     //For Camera
     private static final int CAMERA_REQUEST = 1888;
@@ -48,7 +66,7 @@ public class Edit_Profile_Activity extends AppCompatActivity {
         oldPassword = findViewById(R.id.old_pass);
         newPassword = findViewById(R.id.new_pass);
         confirmNewPassword = findViewById(R.id.confirm_pass);
-
+        db = new editUserDetailsDB();
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +92,23 @@ public class Edit_Profile_Activity extends AppCompatActivity {
 
 
     }
+
+
+    public void setTexts(String email, String phone, String location, String username){
+        Log.d(TAG, "*********----->setTexts");
+
+        EditText e = findViewById(R.id.new_email);
+        EditText p = findViewById(R.id.new_phone);
+        EditText l = findViewById(R.id.new_location);
+        EditText u = findViewById(R.id.new_username);
+        Log.d(TAG, "*********----->"+email);
+        e.setText(email);
+        p.setText(phone);
+        l.setText(location);
+        u.setText(username);
+        Log.d(TAG, "*********----->LEAVING setTexts");
+    }
+
     //Code From https://stackoverflow.com/a/5991757
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -98,7 +133,72 @@ public class Edit_Profile_Activity extends AppCompatActivity {
     }
 
 
+    private void returnToLogin() {
+        startActivity(new Intent(this, Login_Activity.class));
+    }
 
+    public class editUserDetailsDB{
+        private FirebaseAuth mAuth;
+        private String email;
+        private String username;
+        private String location;
+        private String phone;
+        private FirebaseUser user;
+        private DatabaseReference mDatabase;
+        private List<User> userList;
+
+        public editUserDetailsDB() {
+            mAuth = FirebaseAuth.getInstance();
+            user = mAuth.getCurrentUser();
+            userList = new ArrayList<>();
+            if (user != null) {
+                email = user.getEmail();
+                getUserDetails();
+
+            } else {
+                returnToLogin();
+
+            }
+        }
+
+
+        private void saveNewDetails(){
+
+        }
+
+        private void getUserDetails(){
+            mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+            Query query = FirebaseDatabase.getInstance().getReference("Users")
+                    .orderByChild("email")
+                    .equalTo(email);
+
+            Log.d(TAG, "*********----->"+email);
+            query.addListenerForSingleValueEvent(valueEventListener);
+
+        }
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userList.clear();
+                Log.d(TAG, "*********----->onDataChange");
+                if (dataSnapshot.exists()) {
+                    Log.d(TAG, "*********----->exists");
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        //Log.d(TAG, "*********----->"+snapshot.getValue());
+                        User user = snapshot.getValue(User.class);
+                        setTexts(user.getEmail(), user.getPhoneNumber(), user.getLocation(), user.getUserName());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+    }
 
 
 }
