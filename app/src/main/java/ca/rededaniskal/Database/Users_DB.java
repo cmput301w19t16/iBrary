70102fa3590e198e6c259513a6cf4a350ec150ff
Nicholas Bombardieri <nbombard@ualberta.ca>
2019-03-28 14:40:45 -0600

    added following/unfollowing count

--- app/src/main/java/ca/rededaniskal/Database/Users_DB.java
+++ app/src/main/java/ca/rededaniskal/Database/Users_DB.java
@@ -12,6 +12,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 import ca.rededaniskal.BusinessLogic.myCallbackBool;
+import ca.rededaniskal.BusinessLogic.myCallbackInt;
 import ca.rededaniskal.BusinessLogic.myCallbackUser;
 import ca.rededaniskal.EntityClasses.Book_List;
 import ca.rededaniskal.EntityClasses.User;
@@ -24,6 +25,7 @@ public class Users_DB {
     private String email;
     private String phoneNumber;
     private String location;
+    private int followCount;
     private String profilePic;
     private ArrayList<User> friends;
     private Book_List ownedBooks;
@@ -39,17 +41,24 @@ public class Users_DB {
     }
 
     public void getUser(String uid, final myCallbackUser mcb){
-        Query query = mDatabase.orderByChild("UID").equalTo(uid);
+        //Query query = mDatabase.orderByChild("uid").equalTo(uid);
+        Query query =mDatabase.child(uid);
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    userName = dataSnapshot.child("userName").toString();
-                    email = dataSnapshot.child("email").toString();
-                    phoneNumber = dataSnapshot.child("phoneNumber").toString();
-                    location = dataSnapshot.child("location").toString();
-
-                    mcb.onCallback(new User(userName, email, phoneNumber, location));
+/*
+                    userName = dataSnapshot.child("userName").getValue().toString();
+                    String userid = dataSnapshot.child("uid").getValue().toString();
+                    email = dataSnapshot.child("email").getValue().toString();
+                    phoneNumber = dataSnapshot.child("phoneNumber").getValue().toString();
+                    location = dataSnapshot.child("location").getValue().toString();
+                    followCount = dataSnapshot.child("followerCount").getValue(int.class);
+                    user = new User(userName, email, phoneNumber, location);
+                    user.setFollowerCount(followCount);
+                    user.setUID(userid);*/
+                    user = dataSnapshot.getValue(User.class);
+                    mcb.onCallback(user);
 
                 }
             }
