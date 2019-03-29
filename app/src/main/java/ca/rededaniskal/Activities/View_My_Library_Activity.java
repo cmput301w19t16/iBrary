96bbec09c90b56dd282edaba3874693c7ade4b28
Revan MacQueen <revan.macqueen1@gmail.com>
2019-03-29 17:13:40 -0600

    added new activities to show followers

--- app/src/main/java/ca/rededaniskal/Activities/View_My_Library_Activity.java
+++ app/src/main/java/ca/rededaniskal/Activities/View_My_Library_Activity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -63,6 +64,7 @@ public class View_My_Library_Activity extends AppCompatActivity {
         selectedOptions = new boolean[filterOptions.length];
 
         recyclerView = (RecyclerView) findViewById(R.id.DisplayBooks);
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
