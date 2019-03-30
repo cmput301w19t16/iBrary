d98850657451d22ba2f2311a70683709bb5f8184
Delaney <dlothian@ualberta.ca>
2019-03-30 17:07:55 -0600

    Added boolean to exchange object

--- app/src/main/java/ca/rededaniskal/EntityClasses/Exchange.java
+++ app/src/main/java/ca/rededaniskal/EntityClasses/Exchange.java
@@ -4,14 +4,33 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Exchange extends Book_Exchange implements Serializable {
-
+    private boolean ownerScanend;
+    private boolean borrowedScanned;
     public Exchange(String owner, String borrower, String isbn, String bookid, Double lat,
                     Double lng, Date time) {
 
         super(owner, borrower, isbn, bookid, lat, lng, time);
+        ownerScanend = false;
+        borrowedScanned = false;
     }
 
     public Exchange(){}
+
+    public boolean isOwnerScanend() {
+        return ownerScanend;
+    }
+
+    public void setOwnerScanend(boolean ownerScanend) {
+        this.ownerScanend = ownerScanend;
+    }
+
+    public boolean isBorrowedScanned() {
+        return borrowedScanned;
+    }
+
+    public void setBorrowedScanned(boolean borrowedScanned) {
+        this.borrowedScanned = borrowedScanned;
+    }
 
     public void updateBookStatus(){}
 
